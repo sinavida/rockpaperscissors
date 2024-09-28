f66a28f428357489a0907aa93e6d77d2cd3d6be7
sinavida <crzmedina@gmail.com>
2024-09-28 08:31:53 -0400

    Complete rock paper scissor project
    Coded random return of rock paper or scissor on to the console

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function getComputerChoice(min , max){
-    return Math.floor(Math.random() * 3) + 1;
+    const computerChose = Math.floor(Math.random() * 3) + 1;
+if (computerChose === 1){
+    return "Rock";
+}if (computerChose === 2){
+    return "Paper";
+}else{
+    return "Scissors";
+}return;
 }
-
 console.log(getComputerChoice());
