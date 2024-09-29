9093fc27a8d172956047fc642d120c100be56ebb
sinavida <crzmedina@gmail.com>
2024-09-29 07:42:12 -0400

    Create rock paper scissors project.
    Added player choice function to be displayed on console.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getComputerChoice(min , max){
+humanScore = 0
+computerScore = 0
+
+
+
+function getComputerChoice(_min , _max){
     const computerChose = Math.floor(Math.random() * 3) + 1;
 if (computerChose === 1){
     return "Rock";
@@ -9,3 +14,18 @@ if (computerChose === 1){
 }return;
 }
 console.log(getComputerChoice());
+
+
+function getHumanChoice(){
+    let humanChoice = prompt("rock, paper, or scissors");
+    if (humanChoice === "rock"){
+        return ("Rock");
+    }if (humanChoice === "paper"){
+        return ("Paper");
+    }if (humanChoice === "scissor"){
+        return ("Scissor");
+    }else{
+        return ("Try Again!")
+    }
+}
+console.log(getHumanChoice())
